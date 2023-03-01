5c9a7ede7a6291051a72f0867377ee9304992384
Tommy Pham <tommypham@Tommys-MacBook-Air-2.local>
2023-02-28 20:56:24 -0600

    organization

--- roommmatedock/rentsplit/urls.py
+++ roommmatedock/rentsplit/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'rentsplit'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('guest-create-profile', views.guest_create_profile, name='guest-create-profile')
 ]
--- roommmatedock/rentsplit/views.py
+++ roommmatedock/rentsplit/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'rentsplit/index.html')
+
+def guest_create_profile():
+    pass
